a841727baf13d98de2deeed5d80d4c42a83c68b8
cecilcox <34152763+cecilcox@users.noreply.github.com>
2022-04-29 07:43:50 -0400

    Updated argument indexing

--- Labs/Lab 2/Helloname.java
+++ Labs/Lab 2/Helloname.java
@@ -1,6 +1,6 @@
 public class Helloname{
     public static void main(String[] args){
-        System.out.println("Hello " + args[1]);
+        System.out.println("Hello " + args[0]);
         //ToDo: Output the provided name backwards 
 
     }
