f7aa874fb98276b08dc6046e509e988adfea993f
cecilcox <34152763+cecilcox@users.noreply.github.com>
2022-04-29 08:05:20 -0400

    Changed exercise

--- Labs/Lab 2/Helloname.java
+++ Labs/Lab 2/Helloname.java
@@ -1,7 +1,9 @@
 public class Helloname{
     public static void main(String[] args){
         System.out.println("Hello " + args[0]);
-        //ToDo: Output the provided name backwards 
+        //ToDo: Output the length of the provided name.
+
+ 
 
     }
 }
