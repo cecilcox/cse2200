471976322bd12689482ce7fa797c1f0d4e252d4b
cecilcox <34152763+cecilcox@users.noreply.github.com>
2022-04-29 07:39:36 -0400

    Fixed cap bug

--- Labs/Lab 2/Helloname.java
+++ Labs/Lab 2/Helloname.java
@@ -1,6 +1,6 @@
 public class Helloname{
     public static void main(String[] args){
-        system.out.println("Hello " + args[1]);
+        System.out.println("Hello " + args[1]);
         //ToDo: Output the provided name backwards 
 
     }
