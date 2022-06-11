cd5bc3eaf4b982234da2ec91bdad6cfabce4f84a
Cecil Cox <s1505551@sms.ed.ac.uk>
2022-06-11 11:48:04 -0400

    Lab 7

---
+++ Labs/Lab7/dg.pl
@@ -0,0 +1,18 @@
+child(erin).
+child(orla).
+child(michelle).
+child(claire).
+child(james).
+
+parent(mary, erin).
+parent(gerry, erin).
+parent(joe, mary).
+parent(joe, sarah).
+parent(sarah, orla).
+
+friends(erin, orla).
+friends(erin, michelle).
+friends(erin, claire).
+friends(orla, michelle).
+friends(orla, claire).
+friends(michelle, claire).
